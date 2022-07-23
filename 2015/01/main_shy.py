8183afad8c34f0d150ca4e2da9977404d032fa2f
Steffen Hytrek <steffen.hytrek+github@gmail.com>
2022-07-23 15:20:38 +0200

    Removed solutions from puzzle texts, added 2015

---
+++ 2015/01/main_shy.py
@@ -0,0 +1,39 @@
+
+with open('./input.txt') as input_file:
+    INPUT_STRING = input_file.read()
+
+
+def part_1() -> None:
+
+    floor_number = 0
+
+    for count, char in enumerate(INPUT_STRING):
+        if char == "(":
+            floor_number += 1
+        elif char == ")":
+            floor_number -= 1
+        else:
+            print(f"Some kind of error with char (pos: {count}): {char}")
+            break
+    print(f"Final floor is: {floor_number}")
+
+
+def part_2() -> None:
+
+    floor_number = 0
+
+    for count, char in enumerate(INPUT_STRING):
+        if char == "(":
+            floor_number += 1
+        elif char == ")":
+            floor_number -= 1
+        else:
+            print(f"Some kind of error with char (pos: {count}): {char}")
+            break
+        if floor_number == -1:
+            print(f"First time in the basement at char position: {count + 1}")
+            break
+
+
+part_1()
+part_2()
